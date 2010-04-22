72d39dfccdd2ce4b85fee2124596117f49af9535
Michael Delaurentis <mdelaurentis@macbook-2.local>
2010-04-21 22:12:35 -0400

    Added some tests for wrapping vectors and maps

--- src/com/mdelaurentis/coverage.clj
+++ src/com/mdelaurentis/coverage.clj
@@ -5,34 +5,27 @@
 
 (def *lines-covered* (ref (sorted-set)))
 
-(defmacro capture2 [line form]
+(defmacro capture [line form]
   (let [text (with-out-str (prn form))]
     `(do 
        (dosync (commute *lines-covered* conj ~line))
        ~form)))
 
 (defn wrap [thing]
-  (if (and (list? thing)
-           (not (= 'ns (first thing))))
-    (let [wrapped (list 'com.mdelaurentis.coverage/capture2 (:line (meta thing)) (map wrap thing))] 
-      (println "mapped it:" wrapped)
-      (dosync (commute *forms* assoc wrapped false))
-      wrapped)
-    thing))
+  (cond 
+   
+   (and (list? thing) (not (= 'ns (first thing))))
+   `(capture ~(:line (meta thing)) ~(map wrap thing))
+   
+   (vector? thing)
+   (vec (map wrap thing))
 
-(deftest test-wrap
-  (is (= '(capture2 (+ 1 2)))
-      (wrap '(+ 1 2)))
-  (is (= '(capture2 (+ (capture (* 2 3)) (capture (/ 12 3)))))
-      (wrap '(+ (* 2 3) (/ 12 3))))
-  (is (= (wrap '(let [a (+ 1 2)
-                      b (+ 3 4)]
-                  (* a b)))
-         '(capture (let [a (capture (+ 1 2))
-                         b (capture (+ 3 4))]
-                     (capture (* a b)))))))
+   (map? thing)
+   (zipmap (map wrap (keys thing))
+           (map wrap (vals thing)))
 
-(run-tests)
+    :else thing))
+
 (defn instrument [file]
   (reverse
    (with-open [in (LineNumberingPushbackReader. (reader file))]
@@ -42,23 +35,29 @@
            (recur (conj forms (wrap form)))
            forms))))))
 
-(defn capture [form]
-  (dosync (commute *lines-covered* conj (:line (meta form)))
-          (commute *forms-covered* conj form)
-          (commute com.mdelaurentis.coverage/*forms* assoc form true))
-  (eval form))
+(defn line-info [in]
+  (loop [forms nil]
+    (if-let [text (.readLine in)]
+      (let [line (dec (.getLineNumber in))
+            info   {:line line
+                    :text text
+                    :blank? (.isEmpty (.trim text))
+                    :covered? (*lines-covered* line)}]
+        (println "Got info" info)
+        (recur (conj forms info)))
+      (reverse forms))))
+
 
 (defn analyze [file]
-  (binding [*lines-covered* (ref (sorted-set))])
-  (doseq [form (instrument file)]
-    (eval form))
-  (with-open [in (LineNumberingPushbackReader. (reader file))]
-    (loop [line (.readLine in)]
-      (when line
-        (let [sym (if (*lines-covered* (dec (.getLineNumber in))) "+" " ")]
-          (println sym (str line))
-          (recur (.readLine in))))))
-  @*lines-covered*)
-
+  (binding [*lines-covered* (ref (sorted-set))]
+    ;; Eval all the forms in the file to fill up *lines-covered*
+    (doseq [form (instrument file)]
+      (eval form))
+    (with-open [in (LineNumberingPushbackReader. (reader file))]
+      (doseq [info (line-info in)]
+        (let [prefix (cond (:blank? info)   "_"
+                           (:covered? info) "+"
+                           :else            " ")]
+          (println prefix (:text info)))))))
 
 (analyze "/Users/mdelaurentis/src/clojure-test-coverage/src/com/mdelaurentis/sample.clj")
---
+++ test/com/mdelaurentis/test_coverage.clj
@@ -0,0 +1,20 @@
+(ns com.mdelaurentis.test-coverage
+  (:use [clojure test]
+        [com.mdelaurentis.coverage]))
+
+(deftest test-wrap
+  (is (= '(capture (+ 1 2)))
+      (wrap '(+ 1 2)))
+  (is (= '(capture (+ (capture (* 2 3)) (capture (/ 12 3)))))
+      (wrap '(+ (* 2 3) (/ 12 3))))
+  (is (= '(com.mdelaurentis.coverage/capture 
+           15
+           (let [a (com.mdelaurentis.coverage/capture 15 (+ 1 2))
+                 b (com.mdelaurentis.coverage/capture 16 (+ 3 4))]
+             (com.mdelaurentis.coverage/capture 17 (* a b))))
+         (wrap '(let [a (+ 1 2)
+                      b (+ 3 4)]
+                  (* a b)))))
+  (is (= '{:a (com.mdelaurentis.coverage/capture 20 (+ 1 2)) 
+           (com.mdelaurentis.coverage/capture 20 (/ 4 2)) "two"}
+         (wrap '{:a (+ 1 2) (/ 4 2) "two"}))))
