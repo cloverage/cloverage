868c1c255b8f0d1bbdfcd7fcc01d2ba4230b2250
Michael Delaurentis <mdelaurentis@macbook-2.local>
2010-04-29 10:49:53 -0400

    Rename project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-test-coverage "1.0.0-SNAPSHOT"
+(defproject com.mdelaurentis/coverage "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.1.0"] 
                  [org.clojure/clojure-contrib "1.1.0"]]
