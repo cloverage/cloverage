58efc3ce039aa7bb5b0c89d400a022c32b00369a
Michael Delaurentis <mdelaurentis@macbook-2.local>
2010-05-26 13:03:42 -0400

    Use swank-clojure instead of leiningen/lein-swank

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.1.0"] 
                  [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]])
