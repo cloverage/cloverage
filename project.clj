33e1c954a53c80966f8a21b3c4143482e2c1af72
Mike DeLaurentis <delaurentis@gmail.com>
2010-10-22 07:08:42 -0400

    Remove unused dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.mdelaurentis/coverage "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.1.0"] 
-                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dependencies [[org.clojure/clojure "1.2.0"] 
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
