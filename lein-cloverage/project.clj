62f10c20427adf54fec100e7b95ad56b0b886cae
Jacek Lach <jacek@lshift.net>
2013-01-07 14:51:58 +0000

    Rename cloverage-lein-plugin to lein-cloverage.

--- lein-cloverage/project.clj
+++ lein-cloverage/project.clj
@@ -1,4 +1,4 @@
-(defproject cloverage-lein-plugin "1.0.0-SNAPSHOT"
+(defproject lein-cloverage "1.0.0-SNAPSHOT"
   :description "Lein plugin for cloverage"
   :url "https://github.com/JacekLach/cloverage"
   :license {:name "Eclipse Public License"
