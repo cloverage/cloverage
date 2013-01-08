9cc3923944d4c9fa0527ba7155da317bfd25abe7
Alexander Schmolck <alexander@lshift.net>
2013-01-08 17:46:50 +0000

    Remove bogus debugging in lein-plugin

--- lein-cloverage/src/leiningen/cloverage.clj
+++ lein-cloverage/src/leiningen/cloverage.clj
@@ -7,10 +7,9 @@
 
   Specify -o OUTPUTDIR for output, for other options see cloverage."
   [project & args]
-  (println "START")
   (let [source-namespaces (mapcat blt/namespaces-in-dir (:source-paths project))
         test-namespace    (mapcat blt/namespaces-in-dir (:test-paths project))]
-    (println run/run (update-in project [:dependencies]
+    (run/run (update-in project [:dependencies]
                                 conj    ['cloverage "1.0.0-SNAPSHOT"])
            "-m" "cloverage.coverage"
            (concat (mapcat  #(list "-x" %) test-namespace)
