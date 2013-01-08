462b8eec2d0293085b8fc8ee41de3c2229eafe65
Alexander Schmolck <alexander@lshift.net>
2013-01-08 15:49:46 +0000

    Make lein-cloverage infer namespaces

--- lein-cloverage/src/leiningen/cloverage.clj
+++ lein-cloverage/src/leiningen/cloverage.clj
@@ -1,9 +1,18 @@
 (ns leiningen.cloverage
-  (:require [leiningen.run :as run]))
+  (:require [leiningen.run :as run]
+            [bultitude.core :as blt]))
 
 (defn cloverage
-  "I don't do a lot."
+  "Run code coverage on the project.
+
+  Specify -o OUTPUTDIR for output, for other options see cloverage."
   [project & args]
-  (apply run/run (update-in project [:dependencies]
-                            conj    ['cloverage "1.0.0-SNAPSHOT"])
-         "-m" "cloverage.coverage" args))
+  (println "START")
+  (let [source-namespaces (mapcat blt/namespaces-in-dir (:source-paths project))
+        test-namespace    (mapcat blt/namespaces-in-dir (:test-paths project))]
+    (println run/run (update-in project [:dependencies]
+                                conj    ['cloverage "1.0.0-SNAPSHOT"])
+           "-m" "cloverage.coverage"
+           (concat (mapcat  #(list "-x" %) test-namespace)
+                   args
+                   source-namespaces))))
