e762643ae3f96a52cbd51a077dd1d4574637ed70
Alexander Schmolck <alexander@lshift.net>
2013-01-08 18:29:09 +0000

    Fix lein-plugin

--- lein-cloverage/src/leiningen/cloverage.clj
+++ lein-cloverage/src/leiningen/cloverage.clj
@@ -2,15 +2,18 @@
   (:require [leiningen.run :as run]
             [bultitude.core :as blt]))
 
+(defn ns-names-for-dirs [dirs]
+  (map name (mapcat blt/namespaces-in-dir dirs)))
+
 (defn cloverage
   "Run code coverage on the project.
 
   Specify -o OUTPUTDIR for output, for other options see cloverage."
   [project & args]
-  (let [source-namespaces (mapcat blt/namespaces-in-dir (:source-paths project))
-        test-namespace    (mapcat blt/namespaces-in-dir (:test-paths project))]
-    (run/run (update-in project [:dependencies]
-                                conj    ['cloverage "1.0.0-SNAPSHOT"])
+  (let [source-namespaces (ns-names-for-dirs (:source-paths project))
+        test-namespace    (ns-names-for-dirs (:test-paths project))]
+    (apply run/run (update-in project [:dependencies]
+                              conj    ['cloverage "1.0.0-SNAPSHOT"])
            "-m" "cloverage.coverage"
            (concat (mapcat  #(list "-x" %) test-namespace)
                    args
