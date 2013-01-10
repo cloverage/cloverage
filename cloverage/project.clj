d54c35f0fcbc48b40376b5500b12839ec3e8623b
Jacek Lach <jacek@lshift.net>
2013-01-10 16:11:32 +0000

    Update project declarations: licenses, urls, warns

--- cloverage/project.clj
+++ cloverage/project.clj
@@ -1,6 +1,11 @@
 (defproject cloverage "1.0.0-SNAPSHOT"
   :description "Form-level test coverage for clojure."
+  :url "https://www.github.com/lshift/cloverage"
   :main cloverage.coverage
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.2"]
                  [org.clojure/tools.logging "0.2.3"]
--- lein-cloverage/project.clj
+++ lein-cloverage/project.clj
@@ -1,8 +1,9 @@
 (defproject lein-cloverage "1.0.0-SNAPSHOT"
   :description "Lein plugin for cloverage"
-  :url "https://github.com/JacekLach/cloverage"
+  :url "https://github.com/lshift/cloverage"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[cloverage "1.0.0-SNAPSHOT"]
                  [bultitude "0.2.0"]]
   :eval-in-leiningen true)
