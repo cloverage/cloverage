4e8f32a8237287aec1b29f20fde5af2830253107
Jacek Lach <jacek@lshift.net>
2013-01-10 17:40:59 +0000

    Release lein-cloverage 1.0.0.

--- lein-cloverage/project.clj
+++ lein-cloverage/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-cloverage "1.0.0-RC2"
+(defproject lein-cloverage "1.0.0"
   :description "Lein plugin for cloverage"
   :url "https://github.com/lshift/cloverage"
   :scm {:name "git"
@@ -10,6 +10,6 @@
             :distribution :repo
             :comments "same as Clojure"}
   :min-lein-version "2.0.0"
-  :dependencies [[cloverage "1.0.0-RC2"]
+  :dependencies [[cloverage "1.0.0"]
                  [bultitude "0.2.0"]]
   :eval-in-leiningen true)
