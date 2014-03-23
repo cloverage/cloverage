587af39ddde788d0ced3ecf4fa64c76cc116c14b
Jacek Lach <jacekklach@gmail.com>
2014-03-23 19:22:52 -0400

    Release cloverage 1.0.4

--- cloverage/project.clj
+++ cloverage/project.clj
@@ -1,4 +1,4 @@
-(defproject cloverage "1.0.4-SNAPSHOT"
+(defproject cloverage "1.0.4"
   :description "Form-level test coverage for clojure."
   :url "https://www.github.com/lshift/cloverage"
   :scm {:name "git"
