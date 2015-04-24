0638901525d2e8e23aa8f23008eae9077cd9b6f7
Tom Parker <palfrey@tevp.net>
2015-04-24 10:14:25 +0100

    Also don't deploy snapshots for non-master

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-if [ x${TRAVIS_PULL_REQUEST} == xfalse ]; then
+if [ x${TRAVIS_PULL_REQUEST} != xfalse ]; then
+  echo "Building a pull request, so not deploying"
+elif [ x${TRAVIS_BRANCH} != xmaster ]; then
+  echo "On branch '$TRAVIS_BRANCH' which isn't master so not doing a deploy"
+else
   echo "Not a pull request, so deploying"
   (cd cloverage && lein deploy clojars)
   (cd lein-cloverage && lein deploy clojars)
-else
-  echo "Building a pull request, so not deploying"
 fi
