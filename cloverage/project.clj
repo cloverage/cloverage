7e2c9c49f1e846c2707eedc5545603ccc533b7e0
Tom Parker <palfrey@tevp.net>
2015-04-27 10:42:39 +0100

    Add lein-release plugin to cloverage so we can automate releases

--- cloverage/project.clj
+++ cloverage/project.clj
@@ -10,7 +10,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :deploy-repositories [["clojars" {:username :env/clojars_username :password :env/clojars_password}]]
+  :deploy-repositories [["clojars" {:username :env/clojars_username :password :env/clojars_password :sign-releases false}]]
+  :plugins [[lein-release "1.0.6"]]
+  :lein-release {
+    :scm :git ; Because we're not in the top-level directory, so it doesn't auto-detect
+    :deploy-via :clojars
+  }
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.2"]
                  [org.clojure/tools.logging "0.2.3"]
