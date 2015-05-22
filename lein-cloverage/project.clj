c6f3809385e1501908fc8cf974e33d975425ffa0
Tom Parker <palfrey@tevp.net>
2015-05-22 17:08:56 +0100

    Update lein-cloverage version in line with cloverage version

--- lein-cloverage/project.clj
+++ lein-cloverage/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-cloverage "1.0.4-SNAPSHOT"
+(defproject lein-cloverage "1.0.6-SNAPSHOT"
   :description "Lein plugin for cloverage"
   :url "https://github.com/lshift/cloverage"
   :scm {:name "git"
