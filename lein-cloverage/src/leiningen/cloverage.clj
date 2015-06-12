c1058f50ee3fccfa6c01fa1f2246f5083d87b7b3
Matt Kipps <matt.kipps@climate.com>
2015-06-12 02:00:23 -0700

    Modify lein plugin to fix test regex
    Currently, -t does not work via lein plugin because cloverage does not
    modify namespace selection if namespaces are provided via the -x flag.
    -p/-s flags do have this behavior though, so they seem like better
    choices for the plugin. The previous commit modified -p and -s to take
    multiple paths, so now they are sufficient to support the lein plugin.
    
    An example command that can now be run:
    
    ```
    lein cloverage -t ".*v1" # previously tested both myapp.v1 & myapp.v2
    lein cloverage -t ".*v1" # now only tests myapp.v1
    ```

--- lein-cloverage/src/leiningen/cloverage.clj
+++ lein-cloverage/src/leiningen/cloverage.clj
@@ -1,9 +1,5 @@
 (ns leiningen.cloverage
-  (:require [leiningen.run :as run]
-            [bultitude.core :as blt]))
-
-(defn ns-names-for-dirs [dirs]
-  (map name (mapcat blt/namespaces-in-dir dirs)))
+  (:require [leiningen.run :as run]))
 
 (defn get-lib-version []
   (or (System/getenv "CLOVERAGE_VERSION") "RELEASE"))
@@ -14,11 +10,10 @@
   To specify cloverage version, set the CLOVERAGE_VERSION environment variable.
   Specify -o OUTPUTDIR for output directory, for other options see cloverage."
   [project & args]
-  (let [source-namespaces (ns-names-for-dirs (:source-paths project))
-        test-namespace    (ns-names-for-dirs (:test-paths project))]
+  (let [coll-to-args (fn [flag coll] (mapcat #(list flag %) coll))
+        source-paths (coll-to-args "-p" (:source-paths project))
+        test-paths   (coll-to-args "-s" (:test-paths project))]
     (apply run/run (update-in project [:dependencies]
                               conj    ['cloverage (get-lib-version)])
            "-m" "cloverage.coverage"
-           (concat (mapcat  #(list "-x" %) test-namespace)
-                   args
-                   source-namespaces))))
+           (concat source-paths test-paths args))))
