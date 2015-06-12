3ba88925b7e21bf30f0cb6fdaf7b1dc9b9f2bfd1
Matt Kipps <matt.kipps@climate.com>
2015-06-12 09:44:45 -0700

    refactoring

--- lein-cloverage/src/leiningen/cloverage.clj
+++ lein-cloverage/src/leiningen/cloverage.clj
@@ -4,14 +4,16 @@
 (defn get-lib-version []
   (or (System/getenv "CLOVERAGE_VERSION") "RELEASE"))
 
+(defn coll-to-args [flag coll]
+  (mapcat #(list flag %) coll))
+
 (defn cloverage
   "Run code coverage on the project.
 
   To specify cloverage version, set the CLOVERAGE_VERSION environment variable.
   Specify -o OUTPUTDIR for output directory, for other options see cloverage."
   [project & args]
-  (let [coll-to-args (fn [flag coll] (mapcat #(list flag %) coll))
-        source-paths (coll-to-args "-p" (:source-paths project))
+  (let [source-paths (coll-to-args "-p" (:source-paths project))
         test-paths   (coll-to-args "-s" (:test-paths project))]
     (apply run/run (update-in project [:dependencies]
                               conj    ['cloverage (get-lib-version)])
