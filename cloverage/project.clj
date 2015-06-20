af9f3353adbf611fa5e141bfa3f018daf2a4511d
Dominic LoBue <dominic.lobue@gmail.com>
2015-06-20 04:35:46 -0700

    AOT compile tools.reader for version guarantees
    Older versions of clojure.tools.reader increase all line numbers by one.
    This results in odd looking reports where you have comments or blank
    lines showing as missing test coverage or weirder still as being covered
    if you have one of these older versions of clojure.tools.reader being
    pulled in transitively by another dependency.
    
    AOT compilation of tools.reader is necessary because configuring
    :exclusions for tools.reader on those dependencies which pull in the old
    version is ineffective when that dependency is using AOT (case in point:
    riemann). Unfortunately leiningen isn't doing the smart thing of using
    the newer but uncompiled version of a dependency yet. The only work
    around is fragile and untenable with a CI service. See the following
    github issues for more info:
    https://github.com/technomancy/leiningen/issues/1879
    https://github.com/technomancy/leiningen/issues/1858

--- cloverage/project.clj
+++ cloverage/project.clj
@@ -6,6 +6,7 @@
         :url  "https://www.github.com/lshift/cloverage"
         :tag  "HEAD"}
   :main ^:skip-aot cloverage.coverage
+  :aot [clojure.tools.reader]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
