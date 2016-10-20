780d337782c7417bf38ce78f1645ee57237bdf9c
lvh <_@lvh.io>
2016-10-20 11:05:56 -0500

    lein-release plugin: bumped version from 1.0.8 to 1.0.9-SNAPSHOT for next development cycle

--- cloverage/project.clj
+++ cloverage/project.clj
@@ -1,4 +1,4 @@
-(defproject cloverage "1.0.8"
+(defproject cloverage "1.0.9-SNAPSHOT"
   :description "Form-level test coverage for clojure."
   :url "https://www.github.com/cloverage/cloverage"
   :scm {:name "git"
