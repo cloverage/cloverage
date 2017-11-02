31323777ab3dc5d72d2baa1872576899778d7ce7
lvh <_@lvh.io>
2017-11-02 16:06:32 -0500

    lein-release plugin: preparing 1.0.10 release

--- cloverage/project.clj
+++ cloverage/project.clj
@@ -1,4 +1,4 @@
-(defproject cloverage "1.0.10-SNAPSHOT"
+(defproject cloverage "1.0.10"
   :description "Form-level test coverage for clojure."
   :url "https://www.github.com/cloverage/cloverage"
   :scm {:name "git"
