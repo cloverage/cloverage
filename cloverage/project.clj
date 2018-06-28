208ce2c80aa0b07f8a49e21d355401f477cc38b1
Laurens Van Houtven (lvh) <lvh@latacora.com>
2018-06-28 17:37:59 -0500

    lein-release plugin: bumped version from 1.0.11 to 1.0.12-SNAPSHOT for next development cycle

--- cloverage/project.clj
+++ cloverage/project.clj
@@ -1,4 +1,4 @@
-(defproject cloverage "1.0.11"
+(defproject cloverage "1.0.12-SNAPSHOT"
   :description "Form-level test coverage for clojure."
   :url "https://www.github.com/cloverage/cloverage"
   :scm {:name "git"
