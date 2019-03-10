d4a86d9ece8d7452d6dd58167b3e5ff9bbf56fa2
lvh <_@lvh.io>
2019-03-09 18:45:08 -0600

    Version 1.1.1

--- cloverage/project.clj
+++ cloverage/project.clj
@@ -1,4 +1,4 @@
-(defproject cloverage "1.1.1-SNAPSHOT"
+(defproject cloverage "1.1.1"
   :description "Form-level test coverage for clojure."
   :url "https://www.github.com/cloverage/cloverage"
   :scm {:name "git"
