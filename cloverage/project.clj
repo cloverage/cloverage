6804623e9a5d673d25adb2c4fea755ce08a10641
Laurens Van Houtven (lvh) <_@lvh.io>
2022-03-22 13:56:03 -0500

    Version 1.2.4-SNAPSHOT

--- cloverage/project.clj
+++ cloverage/project.clj
@@ -1,4 +1,4 @@
-(defproject cloverage "1.2.3"
+(defproject cloverage "1.2.4-SNAPSHOT"
   :description "Form-level test coverage for clojure."
   :url "https://www.github.com/cloverage/cloverage"
   :scm {:name "git"
