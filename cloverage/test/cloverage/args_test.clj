fdb179080f8c0111a7f62152e4a3c8a4f34e8ac8
vemv <vemv@users.noreply.github.com>
2022-05-23 10:08:49 +0200

    Make validation errors in the project settings fail Cloverage runs
    Fixes https://github.com/cloverage/cloverage/issues/331

--- cloverage/test/cloverage/args_test.clj
+++ cloverage/test/cloverage/args_test.clj
@@ -1,7 +1,8 @@
 (ns cloverage.args-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [are deftest is testing]]
             [cloverage.args :as args]
-            [cloverage.coverage]))
+            [cloverage.coverage])
+  (:import (clojure.lang ExceptionInfo)))
 
 (deftest overwriting
   (let [output (#'args/overwrite
@@ -15,14 +16,7 @@
       (is (= '(1 2 3 4)
              (:coll output)))
       (is (= "new"
-             (:string output))))
-
-    (testing "removing the invalid keys"
-      (let [output (#'args/overwrite
-                    {}
-                    {:test-ns-regex [#"" ""]
-                     :src-ns-path   [1]})]
-        (is (nil? (find output :src-ns-path)))))))
+             (:string output))))))
 
 (deftest fixing-options
   (let [opts (first (args/parse-args ["-p" "src1"] {:src-ns-path ["src2"]}))]
@@ -52,3 +46,39 @@
           "equivalent to the expected symbol")
       (is (= 42 (cloverage.coverage/launch-custom-report crep {:args [42]}))
           "launches the custom reporter"))))
+
+(deftest validate!
+  (are [input expected] (= expected
+                           (try
+                             (args/validate! input)
+                             (catch ExceptionInfo e
+                               (is (= "Invalid project settings"
+                                      (ex-message e)))
+                               (->> e
+                                    ex-data
+                                    :invalid-pairs
+                                    (sort-by :validation-fn)))))
+    {}                                nil
+    {:custom-report 'a}               nil
+    {:custom-report "not a symbol"}   [{:validation-fn 'clojure.core/symbol?
+                                        :v             "not a symbol"
+                                        :k             :custom-report}]
+    {:ns-exclude-regex "not symbols"} [{:validation-fn 'cloverage.args/regexes-or-strings?,
+                                        :v             "not symbols",
+                                        :k             :ns-exclude-regex}]
+    {:custom-report    "not a symbol"
+     :ns-exclude-regex "not symbols"} [{:validation-fn 'clojure.core/symbol?,
+                                        :v             "not a symbol",
+                                        :k             :custom-report}
+                                       {:validation-fn 'cloverage.args/regexes-or-strings?,
+                                        :v             "not symbols",
+                                        :k             :ns-exclude-regex}]))
+
+(deftest parse-args
+  (testing "Uses validation"
+    (is (= {:invalid-pairs
+            [{:k :runner, :v 'midje, :validation-fn `keyword?}]}
+           (try
+             (args/parse-args [] {:runner 'midje})
+             (catch ExceptionInfo e
+               (ex-data e)))))))
