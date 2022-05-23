724d687e45961426170c6d24b0c581ae1704ba18
vemv <vemv@users.noreply.github.com>
2022-05-23 21:28:01 +0200

    Upgrade various dependencies
    Fixes https://github.com/cloverage/cloverage/issues/218
    Fixes https://github.com/cloverage/cloverage/issues/319
    Fixes https://github.com/cloverage/cloverage/issues/334

--- cloverage/project.clj
+++ cloverage/project.clj
@@ -13,25 +13,25 @@
             :comments "same as Clojure"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
-  :dependencies [[org.clojure/tools.reader "1.3.3"]
+  :dependencies [[org.clojure/tools.reader "1.3.6"]
                  [org.clojure/tools.cli "0.4.2"]
                  [org.clojure/tools.logging "0.5.0"]
-                 [org.clojure/tools.namespace "1.0.0"]
+                 [org.clojure/tools.namespace "1.3.0"]
                  [org.clojure/java.classpath "1.0.0"]
-                 [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/data.xml "0.2.0-alpha6"]
+                 [org.clojure/data.json "2.4.0"]
                  [riddley "0.2.0"]
                  [slingshot "0.12.2"]]
   :profiles {:dev {:aot ^:replace []
-                   :dependencies [[org.clojure/clojure "1.10.1"]
+                   :dependencies [[org.clojure/clojure "1.11.1"]
                                   [lambdaisland/kaocha "1.0.887"]
                                   [lambdaisland/kaocha-cloverage "1.0.75"]
-                                  [pjstadig/humane-test-output "0.10.0"]]
+                                  [pjstadig/humane-test-output "0.11.0"]]
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]
-                   :plugins [[lein-cljfmt "0.6.4"]
+                   :plugins [[lein-cljfmt "0.8.0"]
                              [jonase/eastwood "1.2.3"]
-                             [lein-kibit "0.1.7"]]
+                             [lein-kibit "0.1.8"]]
                    :global-vars {*warn-on-reflection* true}
                    :resource-paths ["dev-resources"]
                    :source-paths ["repl" "sample"]}
@@ -41,8 +41,9 @@
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
-             :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
+             :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}
              :test {:source-paths ["sample"]
                     :jvm-opts ["-Duser.language=en-US"]}}
-  :aliases {"all" ["with-profile" "+1.4:+1.5:+1.6:+1.7:+1.8:+1.9:+1.10"]
+  :aliases {"all" ["with-profile" "+1.4:+1.5:+1.6:+1.7:+1.8:+1.9:+1.10:+1.11"]
             "kaocha" ["with-profile" "dev" "run" "-m" "kaocha.runner"]})
