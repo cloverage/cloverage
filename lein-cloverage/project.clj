c7adbae6140f027f1e46f7c9282d254eb92305dc
Laurens Van Houtven (lvh) <_@lvh.io>
2022-05-24 09:14:44 -0500

    Version 1.3.0-SNAPSHOT

--- lein-cloverage/project.clj
+++ lein-cloverage/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-cloverage "1.2.4"
+(defproject lein-cloverage "1.3.0-SNAPSHOT"
   :description "Lein plugin for cloverage"
   :url "https://github.com/cloverage/cloverage"
   :scm {:name "git"
